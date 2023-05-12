a8176cf2bef37ea61ca6cc887fd86e6e6667620a
y2on-ju <123609582+y2on-ju@users.noreply.github.com>
2023-05-12 17:00:07 +0900

    first

---
+++ src/main/java/com/example/demo/AgainJobApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgainJobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgainJobApplication.class, args);
+	}
+
+}
